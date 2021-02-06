386f2bbb83c278377f7dc38da54f69d305526580
Geetha Arul <geetha.arul@tagitmobile.com>
2021-02-06 15:01:49 +0530

    Addeddirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TextResizeDirective } from './text-resize.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextResizeDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
